df57f7638a8b2128ae5e0f0e1ae35b6e37b8d467
Xundan91 <kundansingh023230@gmail.com>
2025-01-09 23:42:06 +0530

    updated schema

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -2,8 +2,12 @@ import express from "express";
 import routes from "./routes";
 import dotenv from "dotenv";
 import cors from "cors"
+import {WebSocket ,WebSocketServer  } from 'ws'
+import http from 'http'
+import { log } from "console";
 dotenv.config();
 var bodyParser = require('body-parser')
+
 
 const app = express();
 
@@ -17,6 +21,43 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api", routes);
  
+const server = http.createServer(function(req, res){
+  res.end("server connnect")
+})
+
+const wss = new WebSocketServer({server})
+
+wss.on("connection", function connection(socket){
+  socket.on("error" , console.error)
+
+  socket.on("message", function foreach(data){
+    wss.clients.forEach(function(client){
+      if( client.readyState === WebSocket.OPEN){
+        client.send(data)
+
+      }
+      
+    })
+  })
+  socket.send("Hello from webserver")
+
+}
+)
+wss.on("connection", function(socket){
+  socket.on("error", console.error)
+  socket.on("message", function client(data){
+    wss.clients.forEach(function(client){
+      if(client.readyState === WebSocket.OPEN){
+        client.send(data)
+      }
+    })
+  })
+})
+server.listen(3000 , ()=>{
+  console.log("server lisening");
+  
+})
+
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
