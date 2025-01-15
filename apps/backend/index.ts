fe250fa9a52b4416e73f1925fcf5e58d5df7b8ec
Xundan91 <kundansingh023230@gmail.com>
2025-01-15 16:58:28 +0530

    port

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -26,7 +26,7 @@ const server = http.createServer(function(req, res){
 })
 
 
-const PORT = process.env.PORT || 8080;
+const PORT =  8080;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
